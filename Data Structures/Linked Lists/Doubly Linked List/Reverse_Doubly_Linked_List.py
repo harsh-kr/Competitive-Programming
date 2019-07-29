f868d75537a69591dfdd3856e0c1fe4ac80f717f
harsh-kr <harshkumar700@gmail.com>
2019-07-29 15:33:53 +0530

    Reversing a doubly linked list

---
+++ Data Structures/Linked Lists/Doubly Linked List/Reverse_Doubly_Linked_List.py
@@ -0,0 +1,118 @@
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.start_node = None
+
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+        else:
+            print("The list is not empty")
+
+    def insert_at_start(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        new_node = Node(data)
+        new_node.nref = self.start_node
+        self.start_node.pref = new_node
+        self.start_node = new_node
+
+    def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        new_node = Node(data)
+        n.nref = new_node
+        new_node.pref = n
+
+    def insert_after_item(self, x, data):
+        if self.start_node is None:
+            print("List is empty")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                if n.item == x:
+                    break
+                n = n.nref
+            if n is None:
+                print("item not in the list")
+            else:
+                new_node = Node(data)
+                new_node.pref = n
+                new_node.nref = n.nref
+                if n.nref is not None:
+                    n.nref.prev = new_node
+                n.nref = new_node
+
+    def insert_before_item(self, x, data):
+        if self.start_node is None:
+            print("List is empty")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                if n.item == x:
+                    break
+                n = n.nref
+            if n is None:
+                print("item not in the list")
+            else:
+                new_node = Node(data)
+                new_node.nref = n
+                new_node.pref = n.pref
+                if n.pref is not None:
+                    n.pref.nref = new_node
+                n.pref = new_node
+
+    def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item, " ")
+                n = n.nref
+
+    def reverse_linked_list(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        p = self.start_node
+        q = p.nref
+        p.nref = None
+        p.pref = q
+        while q is not None:
+            q.pref = q.nref
+            q.nref = p
+            p = q
+            q = q.pref
+        self.start_node = p
+
+
+new_linked_list = DoublyLinkedList()
+new_linked_list.insert_in_emptylist(51)
+new_linked_list.insert_at_start(10)
+new_linked_list.insert_at_start(89)
+new_linked_list.insert_at_end(30)
+new_linked_list.insert_at_end(99)
+new_linked_list.insert_after_item(30, 65)
+new_linked_list.insert_before_item(99, 100)
+
+new_linked_list.traverse_list()
+new_linked_list.reverse_linked_list()
+new_linked_list.traverse_list()
