4fe45a82f5b7bc5a53ee64ed9ae72060cb6a070c
harsh-kr <harshkumar700@gmail.com>
2020-11-03 18:47:12 +0530

    Solution of Angry Professor

---
+++ Angry Professor/angry_professor.py
@@ -0,0 +1,27 @@
+def angryProfessor(k, a):
+    count = 0
+    for i in a:
+        if i <= 0:
+            count += 1
+    if count < k:
+        return "YES"
+    else:
+        return "NO"
+
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        print(result)
